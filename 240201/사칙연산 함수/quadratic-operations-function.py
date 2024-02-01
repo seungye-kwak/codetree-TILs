392f1a0dc204b91a522a26f0a9988f65e8c537ca
sseung_ye <112370282+seungye-kwak@users.noreply.github.com>
2024-02-01 09:19:18 +0900

    Title: 사칙연산 함수, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240201/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,17 @@
+a, o, c = input().split()
+def cal(a, o, c) :
+    if o not in ['+', '-', '*', '/'] :
+        return False 
+    a, c = int(a), int(c)
+    if o == '+' :
+        r = a + c
+    elif o == '-' :
+        r = a - c
+    elif o == '*' :
+        r = a * c
+    else :
+        r = a/c
+
+    return f'{a} {o} {c} = {r}'
+
+print(cal(a,o,c))
