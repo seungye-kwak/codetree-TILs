92b7dd6e21497e055746dfce5d18bc91c071a93c
sseung_ye <112370282+seungye-kwak@users.noreply.github.com>
2024-02-07 08:35:38 +0900

    Title: 두 정수에 대한 연산값 2, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 240207/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -3,6 +3,9 @@ a, b = map(int, input().split())
 def plus_multiple(a,b) :
     f=max(a,b)*2
     s=min(a,b)+10
-    return s, f
+    if max(a,b) == a :
+        return f, s
+    else :
+        return s, f
 
 print(*plus_multiple(a, b))
