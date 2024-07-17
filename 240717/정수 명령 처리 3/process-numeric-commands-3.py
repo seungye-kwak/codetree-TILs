0922f5e930fda02f44320f99d424d8c8113a9e74
sseung_ye <112370282+seungye-kwak@users.noreply.github.com>
2024-07-17 11:30:07 +0900

    Title: 정수 명령 처리 3, Time: 438ms, Memory: 29MB, Status: Passed - CodeTree

--- 240717/정수 명령 처리 3/process-numeric-commands-3.py
+++ 240717/정수 명령 처리 3/process-numeric-commands-3.py
@@ -1,72 +1,26 @@
 from collections import deque
 
-class Deque :
-    def __init__(self) :
-        self.dq = deque()
-
-    def push_front(self, num):
-        self.dq.appendleft(num)
-
-    def push_back(self, num):
-        self.dq.append(num)
-
-    def size(self) :
-        return len(self.dq)
-
-    def empty(self) :
-        if self.size == 0:
-            return 1
-        else :
-            return 0
-
-    def pop_front(self):
-        if self.size==0:
-            raise ValueError
-
-        else:
-            return self.dq.popleft()
-
-    def pop_back(self):
-        if self.size==0:
-            raise ValueError
-
-        else:
-            return self.dq.pop()
-
-    def front(self):
-        return self.dq[0]
-
-    def back(self):
-        return self.dq[-1]
-
+# 변수 선언 및 입력:
 n = int(input())
-dq = Deque()
-for _ in range(n) :
-    req = input()
+dq = deque()
 
-    if 'push' in req:
-        num = req.split(' ')[-1]
-        if 'back' in req:
-            dq.push_back(num)
-
-        elif 'front' in req :
-            dq.push_front(num)
-
-    elif 'pop' in req:
-        if 'back' in req:
-            print(dq.pop_back())
-
-        else :
-            print(dq.pop_front())
-
-    elif 'size' in req:
-        print(dq.size())
-
-    elif 'empty' in req:
-        print(dq.empty())
-
-    elif 'front' in req :
-        print(dq.front())
-
-    elif 'back' in req:
-        print(dq.back())
+for _ in range(n):
+    command = input()
+    if command.startswith("push_front"):
+        x = int(command.split()[1])
+        dq.appendleft(x)
+    elif command.startswith("push_back"):
+        x = int(command.split()[1])
+        dq.append(x)
+    elif command == "pop_front":
+        print(dq.popleft())
+    elif command == "pop_back":
+        print(dq.pop())
+    elif command == "size":
+        print(len(dq))
+    elif command == "empty":
+        print(1 if not dq else 0)
+    elif command == "front":
+        print(dq[0])
+    else:
+        print(dq[-1])
